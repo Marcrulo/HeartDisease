7e8f2e67c518765a3c1eb1fe918e70e96c4d2eb7
Marcrulo <marcus@presutti.eu>
2021-09-14 11:57:45 +0200

    useless comment

--- main.py
+++ main.py
@@ -12,18 +12,37 @@ import pandas as pd
 
 
 
-
 # %% Import data
 
+# Read
 df = pd.read_csv('heart.csv')
-#branch test
+print(df.head())
 
-
+# Split
+data=df.loc[:,'age':'thal']
+target=df.iloc[:,-1]
 
 
 # %% Summary statistics
 
+# MEASURES OF UNCERTAINTY
+print(df.mean()) # mean
+print(df.median()) # median
+print('quantiles') # quantiles
 
+# MEASURES OF SPREAD
+print(df.var()) # variance
+print(df.std()) # std. dev.
+print('coefficient of variation') # <--
+print('inter quartile range') # IQR
+
+# MEASURES OF RELATION
+print('covariance') # cov
+print('correlation') # corr
+
+
+# 'mixed'
+print(df.describe(include='all'))
 
 
 
