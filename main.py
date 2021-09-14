1f215e2975f02d94c3a94a4c259606915afdf2d4
Marcrulo <marcus@presutti.eu>
2021-09-14 14:07:39 +0200

    useless comment

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ Created on Sun Sep 12 15:49:55 2021
 # %% Import packages/libraries
 import numpy as np
 import pandas as pd
-
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
 # %% Import data
@@ -48,17 +49,49 @@ print(df.describe(include='all'))
 
 # %% PCA or something...
 
+# WHAT NEEDS TO BE DESCRIBED
+# the amount of variation explained as a function of the number of PCA components included
+# principal directions of the considered PCA components
+# the data projected onto the considered principal components
 
 
 
+# %% PCA
+pca = PCA(n_components=11)
+principalComponents = pca.fit_transform(data)
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['principal component 1', 'principal component 2'])
+finalDf = pd.concat([principalDf, df[['target']]], axis = 1)
+
+# %% PLOT 2 FIRST PRINCIPCAL COMPONENTS
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1) 
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
+targets = [0,1]
+colors = ['r', 'g', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['target'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid()
 
 
-# %%
+# %% # EXPLAINED VARIANCE
+exp_var_pca = pca.explained_variance_ratio_
+cum_sum_eigenvalues = np.cumsum(exp_var_pca)
 
+# Individual explained variance
+plt.bar(range(0,len(exp_var_pca)), exp_var_pca, alpha=0.5, align='center', label='Individual explained variance')
 
-
-
-
-
-
-
+# Cumulative explained variance
+plt.step(range(0,len(cum_sum_eigenvalues)), cum_sum_eigenvalues, where='mid',label='Cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal component index')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
