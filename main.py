474508c9af5e485fa97a24451410a2283bd63210
Marcrulo <marcus@presutti.eu>
2021-09-16 13:50:21 +0200

    useless comment

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+#from scipy import stats
 
 # %% Import data
 
 # Read
-df = pd.read_csv('heart.csv')
-print(df.head())
+#my_data = np.genfromtxt('heart.csv', names=header, delimiter=',')
+df=pd.read_csv('heart.csv')
 
 # Split
 data=df.loc[:,'age':'thal']
@@ -28,17 +29,19 @@ target=df.iloc[:,-1]
 # MEASURES OF UNCERTAINTY
 print(df.mean()) # mean
 print(df.median()) # median
-print('quantiles') # quantiles
+print(df.quantile(0.25)) # 25% quantile
+print(df.quantile(0.50)) # 50% quantile
+print(df.quantile(0.75)) # 75% quantile
 
 # MEASURES OF SPREAD
 print(df.var()) # variance
 print(df.std()) # std. dev.
-print('coefficient of variation') # <--
-print('inter quartile range') # IQR
+print(df.std()/df.mean()) # <--
+print(df.quantile(0.75)-df.quantile(0.25)) # IQR
 
 # MEASURES OF RELATION
-print('covariance') # cov
-print('correlation') # corr
+print(df.cov()) # cov
+print(df.corr()) # corr
 
 
 # 'mixed'
@@ -56,11 +59,50 @@ print(df.describe(include='all'))
 
 
 # %% PCA
-pca = PCA(n_components=11)
-principalComponents = pca.fit_transform(data)
-principalDf = pd.DataFrame(data = principalComponents
-             , columns = ['principal component 1', 'principal component 2'])
+pca = PCA(n_components=3)
+principalComponents = pca.fit_transform(df)
+#principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2'])
+principalDf = pd.DataFrame(data = principalComponents)
 finalDf = pd.concat([principalDf, df[['target']]], axis = 1)
+
+# %% # EXPLAINED VARIANCE
+exp_var_pca = pca.explained_variance_ratio_
+cum_sum_eigenvalues = np.cumsum(exp_var_pca)
+
+# Individual explained variance
+plt.bar(range(0,len(exp_var_pca)), exp_var_pca, alpha=0.5, align='center', label='Individual explained variance')
+
+# Cumulative explained variance
+plt.step(range(0,len(cum_sum_eigenvalues)), cum_sum_eigenvalues, where='mid',label='Cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal component index')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+# %% Direction of PCA components
+print(pca.components_)
+
+N,M=df.shape
+V=pca.components_.T
+
+pcs = [0,1,2]
+legendStrs = ['PC'+str(e+1) for e in pcs]
+bw = .2
+r = np.arange(1,M+1)
+attributeNames = df.columns.values.tolist()
+
+for i in pcs:    
+    plt.bar(r+i*bw, V[:,i], width=bw)
+plt.xticks(r+bw, attributeNames,rotation=90)
+plt.xlabel('Attributes')
+plt.ylabel('Component coefficients')
+plt.legend(legendStrs)
+plt.grid()
+plt.title('NanoNose: PCA Component Coefficients')
+plt.show()
+
+
 
 # %% PLOT 2 FIRST PRINCIPCAL COMPONENTS
 fig = plt.figure(figsize = (8,8))
@@ -80,17 +122,3 @@ ax.legend(targets)
 ax.grid()
 
 
-# %% # EXPLAINED VARIANCE
-exp_var_pca = pca.explained_variance_ratio_
-cum_sum_eigenvalues = np.cumsum(exp_var_pca)
-
-# Individual explained variance
-plt.bar(range(0,len(exp_var_pca)), exp_var_pca, alpha=0.5, align='center', label='Individual explained variance')
-
-# Cumulative explained variance
-plt.step(range(0,len(cum_sum_eigenvalues)), cum_sum_eigenvalues, where='mid',label='Cumulative explained variance')
-plt.ylabel('Explained variance ratio')
-plt.xlabel('Principal component index')
-plt.legend(loc='best')
-plt.tight_layout()
-plt.show()
