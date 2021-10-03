90f83552ba81a51e83f91ace9f7f9344235521bb
Marcrulo <marcus@presutti.eu>
2021-10-03 18:35:09 +0200

    useless comment

--- main.py
+++ main.py
@@ -191,20 +191,12 @@ plt.show()
 
 
 # %% PLOT 2 FIRST PRINCIPCAL COMPONENTS
-pca = PCA(n_components=2)
-principalComponents = pca.fit_transform(df)
-principalDf = pd.DataFrame(data = principalComponents
-             , columns = ['principal component 1', 'principal component 2'])
-
-finalDf = pd.concat([principalDf, df[['target']]], axis = 1)
-
-
 fig = plt.figure(figsize = (8,8))
 ax = fig.add_subplot(1,1,1) 
 ax.set_xlabel('Principal Component 1', fontsize = 15)
 ax.set_ylabel('Principal Component 2', fontsize = 15)
 ax.set_title('2 component PCA', fontsize = 20)
-targets = ['principal component 1', 'principal component 2']
+targets = [0,1]
 colors = ['r', 'g', 'b']
 for target, color in zip(targets,colors):
     indicesToKeep = finalDf['target'] == target
